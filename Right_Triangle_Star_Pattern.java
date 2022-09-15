55ec4c2ed70dbc2ac92b8ad991e34190ef3eac9b
GOVIND PATHAK <49300083+aadityapathak@users.noreply.github.com>
2022-09-15 11:44:01 +0530

    Add files via upload

---
+++ Right_Triangle_Star_Pattern.java
@@ -0,0 +1,16 @@
+public class JavaExample
+{
+  public static void main(String args[])
+  {
+    int row, column, numberOfRows=8;
+    for(row=0; row<numberOfRows; row++)
+    {
+      for(column=0; column<=row; column++)
+      {
+        System.out.print("* ");
+      }
+      //This is just to print the stars in new line after each row
+      System.out.println();
+    }
+  }
+}
